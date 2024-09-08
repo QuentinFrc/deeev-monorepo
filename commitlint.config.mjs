63fb517ba1cab7c726b6b441550f9b2f9af77bdd
Quentin François <quentin.francois75@gmail.com>
2024-09-08 03:11:53 +0200

    chore: few root repo updates

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,7 +1,29 @@
 const Config = {
-    extends: ['@commitlint/config-conventional'],
-    formatter: '@commitlint/format',
-    rules: {"scope-enum": [1, "always", ['configs', 'ui', 'utils', 'web', 'docs', 'deps']]}
+	extends: ['@commitlint/config-conventional'],
+	formatter: '@commitlint/format',
+	rules: {
+		'scope-enum': [1, 'always', ['configs', 'ui', 'utils', 'web', 'docs', 'deps']],
+		'type-enum': [
+			2,
+			'always',
+			[
+				'feat',
+				'fix',
+				'docs',
+				'style',
+				'refactor',
+				'test',
+				'chore',
+				'revert',
+				'ci',
+				'perf',
+				'build',
+				'release',
+				'wip',
+				'merge',
+			],
+		],
+	},
 };
 
 export default Config;
